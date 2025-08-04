978ade9f72e794e3040116f44e49fe36286b2a26
Vladimir <vovdyadjamlov@gmail.com>
2025-08-04 12:30:07 +0500

    feat: chat schema

---
+++ common/schemas/chat.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from typing import Union
+
+from pydantic import BaseModel
+
+from common.schemas.info_flow import InfoFlowType
+
+
+class ChatBase(BaseModel):
+    id: int
+    info_flow_id: int
+    messenger_chat_id: str
+    company_id: int
+    is_blocked: bool
+    type: InfoFlowType
+    meta: Union[dict, None] = None
